32a9d555e4616ecad1466d01ac4f08f290e2554c
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-02-20 01:32:53 +0530

    files are now uploading from POSTMAN

--- api/file_api/views.py
+++ api/file_api/views.py
@@ -3,17 +3,37 @@ from rest_framework.exceptions import ParseError
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import File
+from rest_framework import status
+from .serializers import FileSerializer
 
 
 class GetNumbers(APIView):
 	parser_classes = [MultiPartParser, FormParser]
 
 	def post(self, request, format=None):
-		print("########### HELLO from POST Request")
-		print("###########file data", request.data)
-		print(request.data["file"])
-		# if "file" not in request.data:
-		# 	raise ParseError("Empty content")
+		# print("######request.FILES[0]", request.FILES['file'])
 
-		# f = request.data["file"]
-		return Response({"Recieved data": request.data})
+		serializer = FileSerializer(data=request.data)
+		print(serializer)
+		if serializer.is_valid():
+			print("serializer is valid")
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+			
+		# print(request.data['file'])
+		if "file" not in request.data:
+			raise ParseError("Empty content")
+
+		# file_object = File(request.FILES["file"])
+		# file_object.save()
+
+		
+		# f = open("media/uploads/text.txt", "r")
+		# file_content = f.read()
+		# print(file_content)
+		# f.close()
+
+		return Response({"Recieved data": request.FILES['file']})
